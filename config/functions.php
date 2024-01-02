fb81c3da2ef422284e0b10000aa73f04c702387d
alan <alangmoreno02@gmail.com>
2024-01-01 23:50:37 -0600

    Init Class added

--- config/functions.php
+++
@@ -1,9 +0,0 @@
-<?php
-class MysqlFunc {
-    function Insert($table, $fields, $values, $connection) {
-        $query = "INSERTO INTO $table($fields) VALUES($values)";
-        $stmt = $connection->prepare($query);
-        return $stmt;
-    }
-}
-?>
--- signup.php
+++ signup.php
@@ -1,49 +1,27 @@
+<?php include("includes/header.php");?>
 <?php
-require "db.php";
-$message="";
-if(isset($_POST['submit'])){
-    if($_POST['userpass'] != $_POST['confirmpass']) {
-        $message = "Password has not been confirmed";
+require "config/Db.php";
+$con = new DatabaseConnect;
+$mes = "";
+if (isset($_POST['submit'])) {
+    if($_POST['confirm'] != $_POST['password']) {
+        $mes = "Password has not been confirmed";
     } else {
-        $status = true;
-        $verifying = $connection->prepare("SELECT * FROM users");
-        $verifying->execute();
-        while($results = $verifying->fetch()) {
-            if($_POST['useremail'] == $results['useremail']) {
-                $status = false;
-                $message = "This user has already been loged up";
-                break;
-            }
-        }
-        if($status == true) {
-            $query = $connection->prepare("INSERT INTO users(useremail, userpass, username) VALUES(:useremail, :userpass, :username)");
-            $query->bindParam(':useremail', $_POST['useremail']);
-            $userpass = password_hash($_POST['userpass'], PASSWORD_BCRYPT);
-            $query->bindParam(':userpass', $userpass);
-            $query->bindParam(':username', $_POST['username']);
-
-            if($query->execute()) {
-                $message = "User has been loged successfully!";
-            }
-        }
+        $mes = $con->insert("users", "email, password, nickname, profilephoto", ":email, :password, :nickname, :profilephoto", "email, password, nickname, profilephoto");
     }
 }
-?>
-<?php include("includes/header.php") ?>
-<?php if(!empty($message) or $message != ""):?>
-<?php if($status == true): ?>
-    <div class="tg tg-success"><?= $message ?></div>
-<?php else:?>
-    <div class="tg tg-danger"><?= $message ?></div>
-<?php endif;?>
-<?php endif;?>
 
+if(!empty($mes)) {
+    echo $mes;
+}
+?>
 <form action="signup.php" method="post">
-    <input type="email" name="useremail" placeholder="e-mail" required>
-    <input type="password" name="userpass" placeholder="password" required>
-    <input type="password" name="confirmpass" placeholder="rewrite your password" required>
-    <input type="text" name="username" placeholder="Nickname (not required)">
+    <input type="email" name="email" placeholder="e-mail" required>
+    <input type="password" name="password" placeholder="password" required>
+    <input type="password" name="confirm" placeholder="rewrite your password" required>
+    <input type="text" name="nickname" placeholder="Nickname (not required)">
+    <input type="text" name="profilephoto" placeholder="photo">
     <input type="submit" name="submit">
 </form>
 
-<?php include("includes/footer.php") ?>
+<?php include("includes/footer.php");?>
